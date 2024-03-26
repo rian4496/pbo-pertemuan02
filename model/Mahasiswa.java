76460ede891824397f4975694154c8c884868204
rian4496 <riangunadi73@gmail.com>
2024-03-26 22:12:10 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa {
+	public static void tampilanInfo (){
+		System.out.println();
+		System.out.println("======== Data  Mahasiswa ======");
+		System.out.println("NPM     : 2210010497");
+		System.out.println("Nama    : Muhammad Rian Gunadi ");
+		System.out.println("===============================");
+	}
+}
